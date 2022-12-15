87262e229ef0b3f2ff938c11ae537a67c7012595
Artemis System <theArtemisSystem@gmail.com>
2022-12-15 16:01:22 +0100

    datagen pack.mcmeta

---
+++ src/Lib/Datagen/PackMcMeta.purs
@@ -0,0 +1,27 @@
+module Lib.Datagen.PackMcMeta where
+
+import Foreign.ReadWrite (class WriteForeign, writeForeign)
+import Lib.Json (unsafeFormatJson)
+import Lib.Serializer (class IsDataType, class Serializable)
+
+newtype PackMcMeta = PackMcMeta
+  { pack_format ∷ Int
+  , description ∷ String
+  }
+
+instance IsDataType PackMcMeta where
+  getFileExtension _ = "mcmeta"
+
+instance WriteForeign PackMcMeta where
+  writeForeign (PackMcMeta pack) = writeForeign { pack }
+
+instance Serializable PackMcMeta where
+  serialize meta = unsafeFormatJson (writeForeign meta)
+
+-- | 1.19.2 pack_format
+resourceMcmeta ∷ String → PackMcMeta
+resourceMcmeta description = PackMcMeta { pack_format: 9, description }
+
+-- | 1.19.2 pack_format
+dataMcmeta ∷ String → PackMcMeta
+dataMcmeta description = PackMcMeta { pack_format: 10, description }
--- src/Main.purs
+++ src/Main.purs
@@ -2,23 +2,42 @@ module Main where
 
 import Prelude hiding ((/))
 
+import Data.Map (SemigroupMap)
+import Data.Map as Map
+import Data.Semigroup.First (First)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Lib.Datagen.PackMcMeta (PackMcMeta, resourceMcmeta)
 import Lib.Serializer (writeData, writeDatum)
+import Lib.Util (toSMap, (/))
 import Node.FS.Aff (rm')
 import QuietLife.Blocks as Blocks
 import QuietLife.Templates (_blocks, _blockstates, _models)
-import Run (runBaseAff)
-import Lib.Util ((/))
+import Run (Run, runBaseAff)
+import Run.Writer (Writer, tellAt)
+import Type.Proxy (Proxy(..))
+import Type.Row (type (+))
+
+_resource_mcmeta ∷ Proxy "resource_mcmeta"
+_resource_mcmeta = Proxy
+
+type ResourceMcMeta r =
+  (resource_mcmeta ∷ Writer (SemigroupMap String (First PackMcMeta)) | r)
 
 main ∷ Effect Unit
 main = launchAff_ do
   rm' "generated"
     { recursive: true, force: true, maxRetries: 0, retryDelay: 100 }
-  Blocks.newBlocks
+  Blocks.newBlocks *> mcmeta
+    # writeData _resource_mcmeta packLocation
     # writeDatum _blocks ("generated" / "content") "kdlycontent"
     # writeData _blockstates (assets / "kdlycontent" / "blockstates")
     # writeData _models (assets / "kdlycontent" / "models")
     # runBaseAff
   where
-  assets = "generated" / "resources" / "core_assets_generated" / "assets"
+  packLocation = "generated" / "resources" / "core_assets_generated"
+  assets = packLocation / "assets"
+
+  mcmeta ∷ ∀ r. Run (ResourceMcMeta + r) Unit
+  mcmeta = tellAt _resource_mcmeta $ toSMap
+    (Map.singleton "pack" $ resourceMcmeta "Quiet Life generated assets")
