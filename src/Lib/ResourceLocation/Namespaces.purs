98057d611f51bda8cc4ac1487e4e36bd4c47c6dd
Artemis System <theArtemisSystem@gmail.com>
2023-02-14 15:42:05 +0100

    add minecraft and quiet_life namespaces

--- src/Lib/ResourceLocation/Namespaces.purs
+++ src/Lib/ResourceLocation/Namespaces.purs
@@ -1,4 +1,10 @@
 module Lib.ResourceLocation.Namespaces where
 
+minecraft ∷ String
+minecraft = "minecraft"
+
 kdlycontent ∷ String
 kdlycontent = "kdlycontent"
+
+quietLife ∷ String
+quietLife = "quiet_life"
