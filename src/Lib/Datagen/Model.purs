4237af33cfdfcab4537a3b7aeab5cdecf5e27fe3
Artemis System <theArtemisSystem@gmail.com>
2023-02-14 15:25:24 +0100

    Use ResourceLocation in Model

--- src/Lib/Datagen/Model.purs
+++ src/Lib/Datagen/Model.purs
@@ -4,15 +4,23 @@ import Prelude
 
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype)
-import Foreign.Object (Object, empty, insert, singleton)
+import Foreign.Object (Object, insert, singleton)
+import Foreign.Object as Object
 import Foreign.ReadWrite (class WriteForeign, writeForeign)
 import Lib.Json (unsafeFormatJson)
+import Lib.ResourceLocation (ResourceLocation)
 import Lib.Serializer (class IsDataType, class Serializable)
+
+data Texture = Texture ResourceLocation | Reference String
+
+instance WriteForeign Texture where
+  writeForeign (Texture rl) = writeForeign rl
+  writeForeign (Reference str) = writeForeign ("#" <> str)
 
 -- | A model that specifies a parent and an object of textures
 newtype Model = Model
-  { parent ∷ String
-  , textures ∷ Maybe (Object String)
+  { parent ∷ ResourceLocation
+  , textures ∷ Maybe (Object Texture)
   }
 
 derive instance Newtype Model _
@@ -24,25 +32,41 @@ instance IsDataType Model where
 instance Serializable Model where
   serialize model = unsafeFormatJson (writeForeign model)
 
-itemModel ∷ String → Model
+itemModel ∷ ResourceLocation → Model
 itemModel parent = Model { parent, textures: Nothing }
 
-all ∷ String → String → Model
+all ∷ ResourceLocation → Texture → Model
 all parent texture = Model { parent, textures: Just $ singleton "all" texture }
 
-pillar ∷ String → { side ∷ String, end ∷ String } → Model
+pillar ∷ ResourceLocation → { side ∷ Texture, end ∷ Texture } → Model
 pillar parent { side, end } = Model
   { parent
-  , textures: empty
+  , textures: Object.empty
       # insert "side" side
       # insert "end" end
       # Just
   }
 
-hollowPillar ∷ String → { end ∷ String, inside ∷ String, side ∷ String } → Model
+directionalPillar
+  ∷ ResourceLocation
+  → { side ∷ Texture, top ∷ Texture, bottom ∷ Texture }
+  → Model
+directionalPillar parent { side, top, bottom } = Model
+  { parent
+  , textures: Object.empty
+      # insert "side" side
+      # insert "top" top
+      # insert "bottom" bottom
+      # Just
+  }
+
+hollowPillar
+  ∷ ResourceLocation
+  → { end ∷ Texture, inside ∷ Texture, side ∷ Texture }
+  → Model
 hollowPillar parent { side, inside, end } = Model
   { parent
-  , textures: empty
+  , textures: Object.empty
       # insert "side" side
       # insert "inside" inside
       # insert "end" end
